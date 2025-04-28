ec24f6d7baa18d7118ad3dc46c5d799b0884b88f
Vineet Mali <7038vineet@gmail.com>
2025-04-28 21:12:41 +0530

    day 1

---
+++ Code.js
@@ -0,0 +1,20 @@
+// Q1 Print numbers from 1 to 10
+
+// for (let i = 0; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+//Q2 Print even numbers between 1 to 50
+
+// for (let i = 0; i <= 50; i += 2) {
+//     console.log(i);
+// }
+
+// Print odd numbers between 1 to 50
+// for (let i = 1; i <= 50; i += 2) {
+//     console.log(i);
+// }
+
+
+
+
