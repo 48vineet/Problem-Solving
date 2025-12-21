4aad98bd2aebce8d537a8b566121ffa7dbb5239e
Vineet Mali <7038vineet@gmail.com>
2025-12-21 22:56:37 +0530

    Add book allocation problem solution with validation and binary search implementation

--- NewDSAStart.js
+++ NewDSAStart.js
@@ -466,3 +466,71 @@
 //   arr[j] = factor;
 // }
 // console.log(arr);
+
+//! Har Level MANNG DSA QUE
+//? Given an array arr[] of integers, where each element arr[i] represents the number of pages in the i-th book. You also have an integer k representing the number of students. The task is to allocate books to each student such that:
+
+//? Each student receives atleast one book.
+//? Each student is assigned a contiguous sequence of books.
+//? No book is assigned to more than one student.
+//? The objective is to minimize the maximum number of pages assigned to any student. In other words, out of all possible allocations, find the arrangement where the student who receives the most pages still has the smallest possible maximum.
+
+//? Note: If it is not possible to allocate books to all students, return -1.
+
+//? Examples:
+
+//? Input: arr[] = [12, 34, 67, 90], k = 2
+//? Output: 113
+//? Explanation: Allocation can be done in following ways:
+//? => [12] and [34, 67, 90] Maximum Pages = 191
+//? => [12, 34] and [67, 90] Maximum Pages = 157
+//? => [12, 34, 67] and [90] Maximum Pages = 113.
+//? The third combination has the minimum pages assigned to a student which is 113.
+
+//**
+//  * @param {number[]} arr
+//  * @param {number} k
+//  * @returns {number}
+//  */
+
+// function isValid(arr, k, maxPages) {
+//   let studentR = 1;
+//   let currPage = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let pages = arr[i];
+//     if (arr[i] + currPage <= maxPages) {
+//       currPage += pages;
+//     } else {
+//       studentR++;
+//       currPage = pages;
+//       if (pages > maxPages) return false;
+//     }
+//   }
+//   return studentR <= k;
+// }
+
+// class Solution {
+//   findPages(arr, k) {
+//     let n = arr.length;
+
+//     if (n < k) {
+//       return -1;
+//     }
+
+//     let start = Math.max(...arr);
+//     let end = arr.reduce((a, b) => a + b, 0);
+//     let ans = -1;
+
+//     while (start <= end) {
+//       let mid = Math.floor((start + end) / 2);
+//       if (isValid(arr, k, mid)) {
+//         ans = mid;
+//         end = mid - 1;
+//       } else {
+//         start = mid + 1;
+//       }
+//     }
+//     return ans;
+//   }
+// }
