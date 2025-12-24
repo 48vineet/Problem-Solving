6fa8f96b1e71b34c3a33fd95765328c73ef43cd4
Vineet Mali <7038vineet@gmail.com>
2025-12-24 22:32:26 +0530

    new que easy

--- NewDSAStart.js
+++ NewDSAStart.js
@@ -571,3 +571,14 @@
 //   }
 //   return ans;
 // };
+
+//! Easy
+//? Given a string s consisting of words and spaces, return the length of the last word in the string.
+//? A word is a maximal substring consisting of non-space characters only.
+
+function lengthOfLastWord(s) {
+  const words = s.trim().split(" ");
+  return words[words.length - 1].length;
+}
+
+console.log(lengthOfLastWord("Hello World")); // 5
