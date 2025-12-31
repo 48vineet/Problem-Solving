7a6bd29a74ba6f51e03d8144ac38b63b3c2d3411
Vineet Mali <7038vineet@gmail.com>
2025-12-31 22:08:37 +0530

    Add implementation for Best Time to Buy and Sell Stock problem

--- NewDSAStart.js
+++ NewDSAStart.js
@@ -629,6 +629,40 @@
 
 //? The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
 
-nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
-let data = new Set(nums);
+// let idx = 1;
+// for (let i = 0; i < nums.length - 1; i++) {
+//   if (nums[i] !== nums[i + 1]) {
+//     nums[idx] = nums[i + 1];
+//     idx++;
+//   }
+// }
+// console.log(nums);
+
+//! 121. Best Time to Buy and Sell Stock
+//? You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+//? You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+//? Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+//? Input: prices = [7,1,5,3,6,4]
+//? Output: 5
+//? Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//? Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+let prices = [7, 1, 5, 3, 6, 4];
+
+let buy = prices[0];
+
+let profit = 0;
+for (let i = 1; i < prices.length; i++) {
+  if (prices[i] < buy) {
+    buy = prices[i];
+    console.log("Buy at day", i);
+  } else if (prices[i] - buy > profit) {
+    profit = prices[i] - buy;
+    console.log("Profit is", profit);
+  }
+}
