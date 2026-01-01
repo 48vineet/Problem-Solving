c560b482ae0ae13ea35758c67e3dda489c79d5ef
Vineet Mali <7038vineet@gmail.com>
2026-01-01 21:50:50 +0530

    Add Rotate Array function implementation

--- NewDSAStart.js
+++ NewDSAStart.js
@@ -652,17 +652,55 @@
 //? Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 //? Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-let prices = [7, 1, 5, 3, 6, 4];
+// let prices = [7, 1, 5, 3, 6, 4];
 
-let buy = prices[0];
+// let buy = prices[0];
 
-let profit = 0;
-for (let i = 1; i < prices.length; i++) {
-  if (prices[i] < buy) {
-    buy = prices[i];
-    console.log("Buy at day", i);
-  } else if (prices[i] - buy > profit) {
-    profit = prices[i] - buy;
-    console.log("Profit is", profit);
+// let profit = 0;
+// for (let i = 1; i < prices.length; i++) {
+//   if (prices[i] < buy) {
+//     buy = prices[i];
+//     console.log("Buy at day", i);
+//   } else if (prices[i] - buy > profit) {
+//     profit = prices[i] - buy;
+//     console.log("Profit is", profit);
+//   }
+// }
+
+//? 189. Rotate Array
+//? Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+//? Example 1:
+//? Input: nums = [1,2,3,4,5,6,7], k = 3
+//? Output: [5,6,7,1,2,3,4]
+//? rotate 1 steps to the right: [7,1,2,3,4,5,6]
+//? rotate 2 steps to the right: [6,7,1,2,3,4,5]
+//? rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+let nums = [1, 2, 3, 4, 5, 6, 7],
+  k = 3;
+
+function reverse(arr, start, end) {
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
   }
 }
+
+var rotate = function (nums, k) {
+  let size = nums.length;
+  let n = k % size;
+  reverse(nums, 0, size - 1);
+  reverse(nums, 0, n - 1);
+  reverse(nums, n, size - 1);
+};
+
+console.log(nums);
