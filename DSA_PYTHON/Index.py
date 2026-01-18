93cdfffd1ea83d5894699fbaf390f42198d9f6bf
Vineet Mali <7038vineet@gmail.com>
2026-01-18 22:34:55 +0530

    Implement palindrome check function in Index.py

--- DSA_PYTHON/Index.py
+++ DSA_PYTHON/Index.py
@@ -35,5 +35,23 @@ rev = 0
 
 # * Method 2
 num = 14683  # Reinitialize num
-mcount = int(log10(num)) + 1
-print(mcount)
+# mcount = int(log10(num)) + 1
+# print(mcount)
+
+
+#! Check if num is palindrome of not
+
+
+def is_palindrome(num):
+    orignal = num
+    rev = 0
+    while num > 0:
+        n = num % 10
+        rev = rev * 10 + n
+        num = num // 10
+    return orignal == rev
+
+
+# Example usage
+num = 121
+print(is_palindrome(num))
