5d3d1a52e6fab43d3eb89bb18a11f8eb9f59cfe1
Vineet Mali <7038vineet@gmail.com>
2026-01-18 19:42:58 +0530

    Add initial implementation for reversing a number in Python

---
+++ DSA_PYTHON/Index.py
@@ -0,0 +1,19 @@
+
+#! Reverse the number python prog
+
+num = 14683
+rev = 0
+
+# for i in range(0, len(str(num))):
+#     n = num % 10
+#     rev = rev * 10 + n
+#     num = num // 10
+
+# print(rev)
+
+# while num > 0:
+#     n = num % 10
+#     rev = rev * 10 + n
+#     num = num // 10
+
+# print(rev)
