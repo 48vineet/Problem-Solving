87fcca05006ffcef63225337d3116beb57338893
Vineet Mali <7038vineet@gmail.com>
2026-01-19 21:28:42 +0530

    Refactor is_palindrome function and update example usage; enhance isAmstrong function for clarity

--- DSA_PYTHON/Index.py
+++ DSA_PYTHON/Index.py
@@ -42,16 +42,40 @@ num = 14683  # Reinitialize num
 #! Check if num is palindrome of not
 
 
-def is_palindrome(num):
+# def is_palindrome(num):
+#     orignal = num
+#     rev = 0
+#     while num > 0:
+#         n = num % 10
+#         rev = rev * 10 + n
+#         num = num // 10
+#     return orignal == rev
+
+
+# # Example usage
+# num = 121
+# print(is_palindrome(num))
+
+#! Check if given no is amstrong no
+
+def isAmstrong(num):
     orignal = num
-    rev = 0
-    while num > 0:
-        n = num % 10
-        rev = rev * 10 + n
-        num = num // 10
-    return orignal == rev
+    sumOf = 0
+    count = 0
+    temp = num  # Store the original number for reuse
+
+    while temp > 0:
+        n = temp % 10
+        count += 1
+        temp = temp // 10
+
+    temp = num  # Reinitialize temp to the original number
+    while temp > 0:
+        n = temp % 10
+        sumOf = sumOf + pow(n, count)
+        temp = temp // 10
+
+    return orignal == sumOf
 
 
-# Example usage
-num = 121
-print(is_palindrome(num))
+print(isAmstrong(134))
