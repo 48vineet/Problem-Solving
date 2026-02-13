bed58fcce4468cfd22cb2608ad558dda1de55adb
Vineet Mali <7038vineet@gmail.com>
2026-02-13 20:02:49 +0530

    Refactor Recursion.py: Comment out unused functions for clarity; retain reverse array function

--- DSA_PYTHON/Recursion.py
+++ DSA_PYTHON/Recursion.py
@@ -23,11 +23,50 @@
 
 #! Sum 1 to n
 
-def func(summ, i, n):
-    if i > n:
-        print(summ)
+# def func(summ, i, n):
+#     if i > n:
+#         print(summ)
+#         return
+#     func(summ+i, i+1, n)
+
+
+# func(0, 1, 10)
+
+#! sum 1 to n by maths
+
+# def func(ft, lt):
+#     n = lt
+#     theSum = n / 2 * (ft + lt)
+#     print(theSum)
+
+
+# func(1, 10)
+
+#! factorial by recursion
+# fact = 1
+
+
+# def func(num):
+#     global fact
+#     if num == 0:
+#         print(fact)
+#         return 1
+#     fact *= num
+#     return func(num - 1)
+
+# func(4)
+
+#! reverse arr no using recursion
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def func(arr, l, r):
+    if l >= r:
+        print(arr)
         return
-    func(summ+i, i+1, n)
+    arr[l], arr[r] = arr[r], arr[l]
+    func(arr, l+1, r-1)
 
 
-func(0, 1, 10)
+func(arr, 0, len(arr)-1)
