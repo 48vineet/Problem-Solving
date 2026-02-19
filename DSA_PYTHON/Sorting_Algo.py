2c4126ca2e66b289dee37a6a30768d1c77f9c679
Vineet Mali <7038vineet@gmail.com>
2026-02-19 11:51:06 +0530

    Update Sorting_Algo.py: Rename Insertion Sort section to Selection Sort and reposition code

--- DSA_PYTHON/Sorting_Algo.py
+++ DSA_PYTHON/Sorting_Algo.py
@@ -1,5 +1,5 @@
 
-#! Insertion Sort
+#! Selection Sort
 
 # arr = [9, 4, 3, 6, 5, 2, 1]
 # n = len(arr)
@@ -26,3 +26,19 @@
 
 
 # print(arr)
+
+#! Insertion Sort
+
+arr = [8, 5, 3, 5, 3, 2, 1]
+
+n = len(arr)
+
+for i in range(1, n):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j+1] = key
+
+print(arr)
