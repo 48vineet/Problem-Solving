213853af23bf4fded5e4e6c53e3fd454a52661b1
Vineet Mali <7038vineet@gmail.com>
2026-02-19 11:13:45 +0530

    Bubble Sort implementations in Sorting_Algo.py for clarity

--- DSA_PYTHON/Sorting_Algo.py
+++ DSA_PYTHON/Sorting_Algo.py
@@ -1,14 +1,28 @@
 
 #! Insertion Sort
 
-arr = [9, 4, 3, 6, 5, 2, 1]
-n = len(arr)
+# arr = [9, 4, 3, 6, 5, 2, 1]
+# n = len(arr)
 
-for i in range(0, n):
-    min_idx = i
-    for j in range(i+1, n):
-        if arr[j] < arr[min_idx]:
-            min_idx = j
-    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+# for i in range(0, n):
+#     min_idx = i
+#     for j in range(i+1, n):
+#         if arr[j] < arr[min_idx]:
+#             min_idx = j
+#     arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
-print(arr)
+# print(arr)
+
+
+#! Bubble sort (Adjacent Swap)
+
+# arr = [9, 4, 3, 6, 5, 2, 1]
+# n = len(arr)
+
+# for i in range(n-2, -1, -1):
+#     for j in range(0, i+1):
+#         if arr[j] > arr[j+1]:
+#             arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+# print(arr)
