b0ec7f8a898761d8c837fc000dd4c3566c6ee4a3
Vineet Mali <7038vineet@gmail.com>
2026-02-24 21:01:21 +0530

    Refactor Revision.py: Comment out sorting algorithms for clarity and fix typo in selection sort header

--- DSA_PYTHON/Revision.py
+++ DSA_PYTHON/Revision.py
@@ -1,16 +1,81 @@
 
-#! Selection sort
+#! Selction sort
+
+# arr = [1, 4, 6, 8, 3]
+# n = len(arr)
+
+# for i in range(0, n):
+#     min_ele = i
+#     for j in range(i+1, n):
+#         if arr[j] < arr[min_ele]:
+#             min_ele = j
+#     arr[i], arr[min_ele] = arr[min_ele], arr[i]
+
+# print(arr)
+
+#! Bubble sort
+
+# arr = [1, 4, 6, 8, 3]
+# n = len(arr)
+
+# for i in range(n-2, -1, -1):
+#     for j in range(0, i+1):
+#         if arr[j] > arr[j+1]:
+#             arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# print(arr)
+
+#! Insertion Sort
+
+# arr = [1, 4, 6, 8, 3]
+# n = len(arr)
+
+# for i in range(0, n):
+#     key = arr[i]
+#     j = i-1
+#     while j >= 0 and arr[j] > key:
+#         arr[j+1] = arr[j]
+#         j -= 1
+#     arr[j+1] = key
+
+# print(arr)
+
+#! merge sort
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    m, n = len(left), len(right)
+
+    while i < m and j < n:
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    if i < m:
+        while i < m:
+            result.append(left[i])
+            i += 1
+
+    if j < n:
+        while j < n:
+            result.append(right[j])
+            j += 1
+
+    return result
+
+
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+    return merge(left, right)
+
 
 arr = [1, 4, 6, 8, 3]
-n = len(arr)
-
-for i in range(0, n):
-    min_idx = i
-    for j in range(i+1, n):
-        if arr[j] < arr[min_idx]:
-            min_idx = j
-    arr[i], arr[min_idx] = arr[min_idx], arr[i]
-
-print(arr)
-
-#! Bubble Sort
+print(mergeSort(arr))
