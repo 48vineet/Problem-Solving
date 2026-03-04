7c081d9b2477aaefcb2cf122d7dab21b2ca23b99
Vineet Mali <7038vineet@gmail.com>
2026-03-04 21:31:02 +0530

    Add implementation for longest consecutive sequence in an array and comment out maximum subarray sum logic

--- DSA_PYTHON/Index.py
+++ DSA_PYTHON/Index.py
@@ -265,15 +265,34 @@ dic = {}
 
 #! leetcode 53
 
-arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-n = len(arr)
-maxi = float("-inf")
-total = 0
+# arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# n = len(arr)
+# maxi = float("-inf")
+# total = 0
 
-for i in range(0, n):
-    total = total + arr[i]
-    maxi = max(total, maxi)
-    if total < 0:
-        total = 0
+# for i in range(0, n):
+#     total = total + arr[i]
+#     maxi = max(total, maxi)
+#     if total < 0:
+#         total = 0
 
-print(maxi)
+# print(maxi)
+
+#! leetcode 128
+
+nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+num_set = set(nums)
+longest_streak = 0
+
+for num in num_set:
+    if num - 1 not in num_set:
+        current_num = num
+        current_streak = 1
+
+        while current_num + 1 in num_set:
+            current_num += 1
+            current_streak += 1
+
+        longest_streak = max(longest_streak, current_streak)
+
+print(longest_streak)
