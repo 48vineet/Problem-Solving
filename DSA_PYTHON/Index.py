f2b3e6ff6845996b6b612bcdd2aa9cabeaea4909
Vineet Mali <7038vineet@gmail.com>
2026-03-07 19:36:52 +0530

    Refactor longest consecutive sequence implementation in Index.py

--- DSA_PYTHON/Index.py
+++ DSA_PYTHON/Index.py
@@ -280,19 +280,18 @@ dic = {}
 
 #! leetcode 128
 
-nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
-num_set = set(nums)
-longest_streak = 0
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        arr = set(nums)
+        longest = 0
 
-for num in num_set:
-    if num - 1 not in num_set:
-        current_num = num
-        current_streak = 1
+        for num in arr:
+            if num - 1 not in arr:
+                count = 1
+                x = num
+                while x + 1 in arr:
+                    count += 1
+                    x += 1
+                longest = max(longest, count)
 
-        while current_num + 1 in num_set:
-            current_num += 1
-            current_streak += 1
-
-        longest_streak = max(longest_streak, current_streak)
-
-print(longest_streak)
+        return longest
