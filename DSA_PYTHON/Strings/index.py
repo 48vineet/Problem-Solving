0bd5a6577c347ee448ac16e274dceceab3580d6f
Vineet Mali <7038vineet@gmail.com>
2026-04-24 22:09:53 +0530

    Refactor index.py: Remove commented-out removeOuterParentheses function and clean up reverseWords implementation

--- DSA_PYTHON/Strings/index.py
+++ DSA_PYTHON/Strings/index.py
@@ -1,14 +1,32 @@
+# class Solution:
+#     def removeOuterParentheses(self, s: str) -> str:
+#         result = ""
+#         count = 0
+#         for ch in s:
+#             if ch == "(":
+#                 count += 1
+#                 if count > 1:
+#                     result += ch
+#             else:
+#                 count -= 1
+#                 if count > 0:
+#                     result += ch
+#         return result
+
+
+s = "    the sky is blue   "
+a = s.strip().split()
+
+ans = a.reverse()
+result = " ".join(ans)
+
+print(result)
+
+
 class Solution:
-    def removeOuterParentheses(self, s: str) -> str:
-        result = ""
-        count = 0
-        for ch in s:
-            if ch == "(":
-                count += 1
-                if count > 1:
-                    result += ch
-            else:
-                count -= 1
-                if count > 0:
-                    result += ch
+    def reverseWords(self, s: str) -> str:
+        a = s.strip().split()
+        a.reverse()
+
+        result = " ".join(a)
         return result
