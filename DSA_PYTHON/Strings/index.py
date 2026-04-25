2a6017813d39ed78bc074993ab40a0cd8cd7e5ed
Vineet Mali <7038vineet@gmail.com>
2026-04-25 22:12:20 +0530

    Add implementation for longestCommonPrefix function to find common prefix in list of strings

--- DSA_PYTHON/Strings/index.py
+++ DSA_PYTHON/Strings/index.py
@@ -30,3 +30,18 @@ class Solution:
 
         result = " ".join(a)
         return result
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result = ""
+        base = strs[0]
+        if len(strs) == 0:
+            return ""
+
+        for i in range(0 , len(base)):
+            for ch in strs[1:]:
+                if i == len(ch) or ch[i] != base[i]:
+                    return result
+            result += base[i]
+
+        return result
