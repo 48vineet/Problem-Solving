8481df70e6f76848a40de35d30555f7129861e58
Vineet Mali <7038vineet@gmail.com>
2026-04-27 23:36:11 +0530

    Add implementation of largestOddNumber function to find the largest odd number in a string

--- DSA_PYTHON/Strings/index.py
+++ DSA_PYTHON/Strings/index.py
@@ -37,3 +37,11 @@ class Solution:
             result += base[i]
 
         return result
+
+
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 == 1:
+                return num[:i+1]
+        return ""
