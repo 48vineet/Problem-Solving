2ad6605ddd01785b36457bbd7bc8dbb05b6b3936
Vineet Mali <7038vineet@gmail.com>
2026-06-05 22:38:53 +0530

    Add decToBinary method to Solution class for converting decimal to binary

---
+++ DSA_PYTHON/Bit Manipulation/index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def decToBinary(self, n):
+
+        res = ""
+        while n > 0:
+            if n % 2 == 1:
+                res += "1"
+            else:
+                res += "0"
+
+            n = n // 2
+
+        return res[::-1]
