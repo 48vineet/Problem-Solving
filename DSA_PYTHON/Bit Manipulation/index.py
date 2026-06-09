44fd691e0470d211baa202cefe32b693002d7911
Vineet Mali <7038vineet@gmail.com>
2026-06-09 23:24:46 +0530

    Add minBitFlips method to Solution class for counting bit flips between two integers

--- DSA_PYTHON/Bit Manipulation/index.py
+++ DSA_PYTHON/Bit Manipulation/index.py
@@ -27,3 +27,13 @@ class Solution:
 	        power += 1
 
 	    return dec
+
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        ans = start ^ goal
+        count = 0
+        for i in range(0 , 32):
+            if ans & (1 << i) != 0:
+                count += 1
+
+        return count
