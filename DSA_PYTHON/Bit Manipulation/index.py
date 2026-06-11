28f87ea2844fe0f118d9b47cf0c4c5a3556bd391
Vineet Mali <7038vineet@gmail.com>
2026-06-11 21:31:21 +0530

    Add hammingWeight method to Solution class for counting the number of 1 bits in an integer

--- DSA_PYTHON/Bit Manipulation/index.py
+++ DSA_PYTHON/Bit Manipulation/index.py
@@ -52,3 +52,12 @@ class Solution:
             result.append(lst)
 
         return result
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        for i in range(0 , 32):
+            if n & (1 << i) != 0:
+                count += 1
+
+        return count
