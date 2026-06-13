7e037a0277cfe3203b284dd9eb46d10dbda7b158
Vineet Mali <7038vineet@gmail.com>
2026-06-14 00:09:54 +0530

    Refactor subsets function to include target sum condition in backtracking

--- DSA_PYTHON/advance_recursion/index.py
+++ DSA_PYTHON/advance_recursion/index.py
@@ -1,15 +1,35 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         result = []
 
-        def backtrack(index, subset):
-            if index == len(nums):
+#         def backtrack(index, subset):
+#             if index == len(nums):
+#                 result.append(subset.copy())
+#                 return
+#             subset.append(nums[index])
+#             backtrack(index+1, subset)
+#             subset.pop()
+#             backtrack(index+1, subset)
+
+#         backtrack(0, [])
+#         return result
+
+def subsets(nums, target):
+    result = []
+
+    def backtrack(index, subset):
+        if index == len(nums):
+            if sum(subset) == target:
                 result.append(subset.copy())
                 return
-            subset.append(nums[index])
-            backtrack(index+1, subset)
-            subset.pop()
-            backtrack(index+1, subset)
+            return
+        subset.append(nums[index])
+        backtrack(index+1, subset)
+        subset.pop()
+        backtrack(index+1, subset)
 
-        backtrack(0, [])
-        return result
+    backtrack(0, [])
+    return result
+
+
+print(subsets([1, 2, 3, 4, 5, 6, 7], 10))
