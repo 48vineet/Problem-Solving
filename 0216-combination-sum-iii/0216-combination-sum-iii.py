15b048ffe3356899c9f1a715a0d5a7c60d197de8
Vineet Mali <7038vineet@gmail.com>
2026-06-28 23:03:09 +0530

    Time: 1 ms (27.94%), Space: 19.3 MB (71.37%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+
+        def solve(idx , subset):
+            if sum(subset) == n and len(subset) == k:
+                res.append(subset.copy())
+                return
+            
+            if idx <= 9:
+
+                subset.append(idx)
+                solve(idx+1 , subset)
+                subset.pop()
+                solve(idx + 1 , subset)
+            return
+        
+        solve(1 , [])
+        return res
+
