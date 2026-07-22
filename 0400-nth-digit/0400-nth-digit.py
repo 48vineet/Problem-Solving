38c4748504da32c7046d64c3d48d16e1513d1fe9
Vineet Mali <7038vineet@gmail.com>
2026-07-23 00:15:32 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.9%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.py
+++ 0400-nth-digit/0400-nth-digit.py
@@ -1,19 +1,15 @@
 class Solution:
     def findNthDigit(self, n: int) -> int:
-        length = 1   # digit length
-        count = 9    # count of numbers with 'length' digits
-        start = 1    # starting number of this range
+        length = 1
+        count = 9
+        start = 1
         
-        # Step 1: find the range
         while n > length * count:
             n -= length * count
             length += 1
             count *= 10
             start *= 10
         
-        # Step 2: find the exact number
         num = start + (n - 1) // length
-        
-        # Step 3: find the digit inside the number
         digit_index = (n - 1) % length
         return int(str(num)[digit_index])
