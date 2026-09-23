ae9d7ca1e27b6a8e2c011c441bed27adc7df6e3b
Vineet Mali <7038vineet@gmail.com>
2026-09-23 22:16:39 +0530

    Time: 0 ms (100%), Space: 20.5 MB (42.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,8 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         hashSet = {}
-
-        for i in range(0 , len(nums)):
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+            if complement in hashSet:
+                return [hashSet[complement], i]
             hashSet[nums[i]] = i
-
-        for i in range(0 , len(nums)):
-           ans = target - nums[i]
-           if ans  in hashSet and hashSet[ans] != i:
-                return [ i , hashSet[ans]]
